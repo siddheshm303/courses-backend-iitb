49e5ef648a4a76bf8b2be9a6efae11857920f916
siddheshm303 <siddheshm303@gmail.com>
2025-06-19 18:08:26 +0530

    First Commit

---
+++ src/main/java/com/example/coursesBackend/CoursesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coursesBackend/CoursesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
