b8d10bd1ca9533e89bb8131bbbc57a862fdd4c72
siddheshm303 <siddheshm303@gmail.com>
2025-06-20 10:22:52 +0530

    DELETE byId

--- src/main/java/com/example/coursesBackend/controller/CourseController.java
+++ src/main/java/com/example/coursesBackend/controller/CourseController.java
@@ -5,13 +5,13 @@ import com.example.coursesBackend.repository.CourseInstanceRepository;
 import com.example.coursesBackend.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -47,13 +47,26 @@ public class CourseController {
 
     //GET /api/courses/{id}
     @GetMapping("/courses/{id}")
-    public ResponseEntity<?> getCourse(@PathVariable String id){
+    public ResponseEntity<?> getCourse(@PathVariable String id) {
         return courseRepo.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found"));
+                .<ResponseEntity<?>>map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Course not found"));
     }
 
+
     //DELETE /api/courses/{id}
+    @DeleteMapping("courses/{id}")
+    public ResponseEntity<?> deleteCourse(@PathVariable String id) {
+        Optional<Course> course = courseRepo.findById(id);
+        if(course.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found");
+        if(courseRepo.existsByPrerequisitesContaining(course.get())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("Cannot delete course: it is a prerequisite for other course");
+        }
+        courseRepo.deleteById(id);
+        return ResponseEntity.ok("Course Deleted Successfully");
+    }
 
     //POST /api/instances
 
