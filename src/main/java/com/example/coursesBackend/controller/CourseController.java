9a32e691bc9544e01e7c67443a079aa835beb4f2
siddheshm303 <siddheshm303@gmail.com>
2025-06-21 14:02:29 +0530

    Add CORS for support frontend integration

--- src/main/java/com/example/coursesBackend/controller/CourseController.java
+++ src/main/java/com/example/coursesBackend/controller/CourseController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class CourseController {
