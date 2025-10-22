195e2cae454a7ec155b2c7b63dacc41d97575a86
siddheshm303 <siddheshm303@gmail.com>
2025-10-22 20:48:42 +0530

    Allow CORS from all origins

--- src/main/java/com/example/coursesBackend/controller/CourseController.java
+++ src/main/java/com/example/coursesBackend/controller/CourseController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class CourseController {
