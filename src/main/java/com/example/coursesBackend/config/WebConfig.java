78ac9b5114d626e6e0d03d0b45b4c36d6cf33d93
siddheshm303 <siddheshm303@gmail.com>
2025-10-25 20:58:46 +0530

    update CORS configuration to allow all origins

--- src/main/java/com/example/coursesBackend/config/WebConfig.java
+++ src/main/java/com/example/coursesBackend/config/WebConfig.java
@@ -14,8 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173",
-                        "http://65.1.64.212:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
